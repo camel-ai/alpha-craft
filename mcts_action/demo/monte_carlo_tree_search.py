bf5b457ca4d70e99978e0251d219615dade38878
Richie Lo <richielyl@hotmail.com>
2024-11-25 21:51:55 +0000

    with depth and no of expansion nodes

--- mcts_action/demo/monte_carlo_tree_search.py
+++ mcts_action/demo/monte_carlo_tree_search.py
@@ -1,25 +1,25 @@
-"""
-A minimal implementation of Monte Carlo tree search (MCTS) in Python 3
-Luke Harold Miles, July 2019, Public Domain Dedication
-See also https://en.wikipedia.org/wiki/Monte_Carlo_tree_search
-https://gist.github.com/qpwo/c538c6f73727e254fdc7fab81024f6e1
-"""
 from abc import ABC, abstractmethod
 from collections import defaultdict
 import math
 
 
 class MCTS:
-    "Monte Carlo tree searcher. First rollout the tree then choose a move."
+    """
+    Monte Carlo tree searcher. First rollout the tree then choose a move.
+    """
 
-    def __init__(self, exploration_weight=1):
+    def __init__(self, exploration_weight=1, max_depth=None, max_expansion=None):
         self.Q = defaultdict(int)  # total reward of each node
         self.N = defaultdict(int)  # total visit count for each node
         self.children = dict()  # children of each node
         self.exploration_weight = exploration_weight
+        self.max_depth = max_depth  # Maximum depth for rollouts
+        self.max_expansion = max_expansion  # Maximum number of children to expand
 
     def choose(self, node):
-        "Choose the best successor of node. (Choose a move in the game)"
+        """
+        Choose the best successor of node. (Choose a move in the game)
+        """
         if node.is_terminal():
             raise RuntimeError(f"choose called on terminal node {node}")
 
@@ -34,20 +34,27 @@ class MCTS:
         return max(self.children[node], key=score)
 
     def do_rollout(self, node):
-        "Make the tree one layer better. (Train for one iteration.)"
+        """
+        Make the tree one layer better. (Train for one iteration.)
+        """
         path = self._select(node)
         leaf = path[-1]
         self._expand(leaf)
-        reward = self._simulate(leaf)
+        reward = self._simulate(leaf, depth=0)
         self._backpropagate(path, reward)
 
     def _select(self, node):
-        "Find an unexplored descendent of `node`"
+        """
+        Find an unexplored descendent of `node`.
+        """
         path = []
+        depth = 0
         while True:
             path.append(node)
+            if self.max_depth is not None and depth >= self.max_depth:
+                return path
             if node not in self.children or not self.children[node]:
-                # node is either unexplored or terminal
+                # Node is either unexplored or terminal
                 return path
             unexplored = self.children[node] - self.children.keys()
             if unexplored:
@@ -55,40 +62,56 @@ class MCTS:
                 path.append(n)
                 return path
             node = self._uct_select(node)  # descend a layer deeper
+            depth += 1
 
     def _expand(self, node):
-        "Update the `children` dict with the children of `node`"
+        """
+        Update the `children` dict with the children of `node`.
+        Only expand up to `max_expansion` nodes if specified.
+        """
         if node in self.children:
-            return  # already expanded
-        self.children[node] = node.find_children()
+            return  # Already expanded
+        children = node.find_children()
+        if self.max_expansion is not None:
+            children = set(list(children)[: self.max_expansion])  # Limit expansions
+        self.children[node] = children
 
-    def _simulate(self, node):
-        "Returns the reward for a random simulation (to completion) of `node`"
+    def _simulate(self, node, depth=0):
+        """
+        Returns the reward for a random simulation (to completion) of `node`.
+        Simulations are terminated early if `max_depth` is reached.
+        """
         invert_reward = True
         while True:
-            if node.is_terminal():
+            if node.is_terminal() or (self.max_depth is not None and depth >= self.max_depth):
                 reward = node.reward()
                 return 1 - reward if invert_reward else reward
             node = node.find_random_child()
             invert_reward = not invert_reward
+            depth += 1
 
     def _backpropagate(self, path, reward):
-        "Send the reward back up to the ancestors of the leaf"
+        """
+        Send the reward back up to the ancestors of the leaf.
+        """
         for node in reversed(path):
             self.N[node] += 1
             self.Q[node] += reward
             reward = 1 - reward  # 1 for me is 0 for my enemy, and vice versa
 
     def _uct_select(self, node):
-        "Select a child of node, balancing exploration & exploitation"
-
+        """
+        Select a child of node, balancing exploration & exploitation.
+        """
         # All children of node should already be expanded:
         assert all(n in self.children for n in self.children[node])
 
         log_N_vertex = math.log(self.N[node])
 
         def uct(n):
-            "Upper confidence bound for trees"
+            """
+            Upper confidence bound for trees.
+            """
             return self.Q[n] / self.N[n] + self.exploration_weight * math.sqrt(
                 log_N_vertex / self.N[n]
             )
